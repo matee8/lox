df77d62cc6f6a40684992a5b43e32ddcd111d082
matee8 <molnarmatenorbert@gmail.com>
2024-09-22 22:47:39 +0200

    14.4: Disassembling Chunks

---
+++ clox/src/debug.c
@@ -0,0 +1,35 @@
+#include "clox/debug.h"
+
+#include <stddef.h>
+#include <stdio.h>
+
+static inline size_t simple_instruction(const char *name, size_t offset)
+{
+	printf("%s\n", name);
+	return offset + 1;
+}
+
+static size_t disassemble_instruction(const struct chunk *c, size_t offset)
+{
+	printf("%04lu ", offset);
+
+	enum opcode instruction = c->codes[offset];
+	switch (instruction) {
+	case OP_RETURN:
+		return simple_instruction("OP_RETURN", offset);
+	default:
+		printf("Unknown opcode %d\n", instruction);
+		return offset + 1;
+	}
+}
+
+void __debug_disassemble_chunk(const struct chunk *c, const char *name)
+{
+	printf("== %s ==\n", name);
+
+	size_t offset = 0;
+
+	while (offset < c->len) {
+		offset = disassemble_instruction(c, offset);
+	}
+}
--- clox/src/main.c
+++ clox/src/main.c
@@ -1,6 +1,16 @@
 #include <stdlib.h>
 
+#include "clox/chunk.h"
+#include "clox/debug.h"
+
 int main(void)
 {
+	struct chunk c;
+
+	chunk_init(&c);
+	chunk_write(&c, OP_RETURN);
+	__debug_disassemble_chunk(&c, "test chunk");
+	chunk_free(&c);
+
 	return EXIT_SUCCESS;
 }
