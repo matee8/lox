e1d4aacf39dc5c18c9d5d80a7b069a5c2343f7fd
matee8 <molnarmatenorbert@gmail.com>
2024-09-22 22:33:14 +0200

    14.3: Chunks of Instructions

---
+++ clox/src/chunk.c
@@ -0,0 +1,30 @@
+#include "clox/chunk.h"
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "clox/memory.h"
+
+void chunk_init(struct chunk *c)
+{
+	c->len = 0;
+	c->cap = 0;
+	c->codes = NULL;
+}
+
+void chunk_write(struct chunk *c, uint8_t byte)
+{
+	if (c->cap < c->len + 1) {
+		c->cap = GROW_CAP(c->cap);
+		c->codes = GROW_ARR(uint8_t, c->codes, c->cap);
+	}
+
+	c->codes[c->len] = byte;
+	c->len++;
+}
+
+void chunk_free(struct chunk *c)
+{
+	FREE_ARR(uint8_t, c->codes);
+	chunk_init(c);
+}
---
+++ clox/src/memory.c
@@ -0,0 +1,17 @@
+#include "clox/memory.h"
+
+#include <stddef.h>
+#include <stdlib.h>
+
+void *reallocate(void *ptr, size_t new_len)
+{
+	if (new_len == 0) {
+		free(ptr);
+		return NULL;
+	}
+
+	void *res = realloc(ptr, new_len);
+	if (res == NULL)
+		exit(1);
+	return res;
+}
