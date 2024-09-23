ee950395c921966d5dcffaa5cab88e4139bc402c
matee8 <graves-bluff-pesky@duck.com>
2024-09-23 12:05:37 +0200

    14.6: Line Information

--- clox/include/clox/chunk.h
+++ clox/include/clox/chunk.h
@@ -15,11 +15,12 @@ typedef struct {
     size_t len;
     size_t cap;
     uint8_t *codes;
+    int32_t *lines;
     value_array constants;
 } chunk;
 
 void chunk_init(chunk *c);
-void chunk_write(chunk *c, uint8_t byte);
+void chunk_write(chunk *c, uint8_t byte, int32_t line);
 void chunk_free(chunk *c);
 size_t chunk_add_constant(chunk *c, value val);
 
