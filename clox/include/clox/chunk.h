6baed3114cec5ad9e9223ba0198dcc14ef74e7d1
matee8 <molnarmatenorbert@gmail.com>
2024-09-23 09:21:55 +0200

    14.5: Constants

--- clox/include/clox/chunk.h
+++ clox/include/clox/chunk.h
@@ -4,7 +4,10 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include "clox/value.h"
+
 typedef enum {
+    OP_CONSTANT,
     OP_RETURN
 } opcode;
 
@@ -12,10 +15,12 @@ typedef struct {
     size_t len;
     size_t cap;
     uint8_t *codes;
+    value_array constants;
 } chunk;
 
 void chunk_init(chunk *c);
 void chunk_write(chunk *c, uint8_t byte);
 void chunk_free(chunk *c);
+size_t chunk_add_constant(chunk *c, value val);
 
 #endif
--- clox/src/chunk.c
+++ clox/src/chunk.c
@@ -4,12 +4,14 @@
 #include <stdint.h>
 
 #include "clox/memory.h"
+#include "clox/value.h"
 
 void chunk_init(chunk *c)
 {
 	c->len = 0;
 	c->cap = 0;
 	c->codes = NULL;
+	value_array_init(&c->constants);
 }
 
 void chunk_write(chunk *c, uint8_t byte)
@@ -20,11 +22,18 @@ void chunk_write(chunk *c, uint8_t byte)
 	}
 
 	c->codes[c->len] = byte;
-	c->len++;
+	++c->len;
 }
 
 void chunk_free(chunk *c)
 {
 	FREE_ARR(uint8_t, c->codes);
+	value_array_free(&c->constants);
 	chunk_init(c);
+}
+
+size_t chunk_add_constant(chunk *c, value val)
+{
+	value_array_write(&c->constants, val);
+	return c->constants.len - 1;
 }
