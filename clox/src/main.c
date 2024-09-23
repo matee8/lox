b2fac5116a94e13540d441455f4868f5fe5b2115
matee8 <graves-bluff-pesky@duck.com>
2024-09-23 21:30:04 +0200

    15.3: An Arithmetic Calculator

--- clox/src/main.c
+++ clox/src/main.c
@@ -9,17 +9,37 @@
 int main(void)
 {
 	chunk c;
-    virtual_machine vm;
-    
-    virtual_machine_init(&vm);
+	virtual_machine vm;
+
+	virtual_machine_init(&vm);
 
 	chunk_init(&c);
+
 	size_t const_idx = chunk_add_constant(&c, 1.2);
 	chunk_write(&c, OP_CONSTANT, 123);
 	chunk_write(&c, (uint8_t)const_idx, 123);
+
+	const_idx = chunk_add_constant(&c, 3.4);
+	chunk_write(&c, OP_CONSTANT, 123);
+	chunk_write(&c, const_idx, 123);
+
+	chunk_write(&c, OP_ADD, 123);
+
+	const_idx = chunk_add_constant(&c, 5.6);
+	chunk_write(&c, OP_CONSTANT, 123);
+	chunk_write(&c, const_idx, 123);
+
+	chunk_write(&c, OP_DIVIDE, 123);
+	chunk_write(&c, OP_NEGATE, 123);
+
 	chunk_write(&c, OP_RETURN, 123);
+
+#ifdef DEBUG_TRACE_EXECUTION
 	__debug_disassemble_chunk(&c, "test chunk");
-    virtual_machine_interpret(&vm, &c);
+#endif
+
+	virtual_machine_interpret(&vm, &c);
+
 	chunk_free(&c);
 
 	return EXIT_SUCCESS;
