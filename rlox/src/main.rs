d3c5c54ff50c0275f0476d755dcf9022d3a718d5
matee8 <graves-bluff-pesky@duck.com>
2024-09-24 16:44:51 +0200

    15. A Virtual Machine - rlox

--- rlox/src/main.rs
+++ rlox/src/main.rs
@@ -1,8 +1,18 @@
-use rlox::chunk::{Chunk, OpCode};
+use rlox::{
+    chunk::{Chunk, OpCode},
+    vm::Vm,
+};
 
 fn main() {
     let mut c = Chunk::new();
-    c.write_opcode(OpCode::OpReturn, 123);
     c.write_constant(1.2, 123);
+    c.write_constant(3.4, 123);
+    c.write_opcode(OpCode::Add, 123);
+    c.write_constant(5.6, 123);
+    c.write_opcode(OpCode::Divide, 123);
+    c.write_opcode(OpCode::Negate, 123);
+    c.write_opcode(OpCode::Return, 123);
     println!("{c:?}");
+    let mut vm = Vm::new(&c);
+    vm.interpret().unwrap();
 }
