c94ad3cace1ddbbde2c3e594230eae7764fae000
matee8 <molnarmatenorbert@gmail.com>
2024-09-25 15:37:32 +0200

    16.2: A Token at a Time

--- clox/include/clox/scanner.h
+++ clox/include/clox/scanner.h
@@ -1,14 +1,66 @@
 #ifndef CLOX_SCANNER_H
 #define CLOX_SCANNER_H
 
+#include <stddef.h>
 #include <stdint.h>
 
+typedef enum {
+	TOKEN_LEFT_PAREN,
+	TOKEN_RIGHT_PAREN,
+	TOKEN_LEFT_BRACE,
+	TOKEN_RIGHT_BRACE,
+	TOKEN_COMMA,
+	TOKEN_DOT,
+	TOKEN_MINUS,
+	TOKEN_PLUS,
+	TOKEN_SEMICOLON,
+	TOKEN_SLASH,
+	TOKEN_STAR,
+	TOKEN_BANG,
+	TOKEN_BANG_EQUAL,
+	TOKEN_EQUAL,
+	TOKEN_EQUAL_EQUAL,
+	TOKEN_GREATER,
+	TOKEN_GREATER_EQUAL,
+	TOKEN_LESS,
+	TOKEN_LESS_EQUAL,
+	TOKEN_IDENTIFIER,
+	TOKEN_STRING,
+	TOKEN_NUMBER,
+	TOKEN_AND,
+	TOKEN_CLASS,
+	TOKEN_ELSE,
+	TOKEN_FALSE,
+	TOKEN_FOR,
+	TOKEN_FUN,
+	TOKEN_IF,
+	TOKEN_NIL,
+	TOKEN_OR,
+	TOKEN_PRINT,
+	TOKEN_RETURN,
+	TOKEN_SUPER,
+	TOKEN_THIS,
+	TOKEN_TRUE,
+	TOKEN_VAR,
+	TOKEN_WHILE,
+	TOKEN_ERROR,
+	TOKEN_EOF
+} token_type;
+
 typedef struct {
-    const char *start;
-    const char *current;
-    int32_t line;
+	token_type type;
+	const char *start;
+	size_t len;
+	int32_t line;
+} token;
+
+typedef struct {
+	const char *start;
+	const char *current;
+	int32_t line;
 } scanner;
 
 void scanner_init(scanner *sc, const char *src);
+token scan_token(scanner *sc);
 
 #endif
--- clox/src/scanner.c
+++ clox/src/scanner.c
@@ -1,8 +1,42 @@
 #include "clox/scanner.h"
 
+#include <string.h>
+
 void scanner_init(scanner *sc, const char *src)
 {
     sc->start = src;
     sc->current = src;
     sc->line = 1;
+}
+
+static uint8_t is_at_end(const scanner *sc) {
+    return *sc->current == '\0';
+}
+
+static token make_token(const scanner *sc, token_type type) {
+    token t;
+    t.type = type;
+    t.start = sc->start;
+    t.len = (size_t)(sc->current - sc->start);
+    t.line = sc->line;
+    return t;
+}
+
+static token error_token(const scanner *sc, const char *msg) {
+    token t;
+    t.type = TOKEN_ERROR;
+    t.start = msg;
+    t.len = (size_t)strlen(msg);
+    t.line = sc->line;
+    return t;
+}
+
+token scan_token(scanner *sc)
+{
+    sc->start = sc->current;
+
+    if (is_at_end(sc))
+        return make_token(sc, TOKEN_EOF);
+
+    return error_token(sc, "Unexpected character.");
 }
