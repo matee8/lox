455a8757e33c38dd0e58ca9bea2397817ef0f5cf
matee8 <molnarmatenorbert@gmail.com>
2024-09-30 12:47:43 +0200

    16.3: A Lexical Grammar for Lox - rlox

--- rlox/src/scanner.rs
+++ rlox/src/scanner.rs
@@ -71,17 +71,76 @@ impl<'src> Scanner<'src> {
 
     #[inline]
     pub fn scan_token(&mut self) -> Token<'src> {
+        self.skip_whitespace();
         self.start = self.current;
 
-        if self.is_at_end() {
-            self.make_token(TokenType::Eof)
-        } else {
-            self.error_token("Unexpected character.")
+        let Some(char) = self.advance() else {
+            return self.make_token(TokenType::Eof);
+        };
+
+        match char {
+            '(' => self.make_token(TokenType::LeftParen),
+            ')' => self.make_token(TokenType::RightParen),
+            '{' => self.make_token(TokenType::LeftBrace),
+            '}' => self.make_token(TokenType::RightBrace),
+            ';' => self.make_token(TokenType::Semicolon),
+            ',' => self.make_token(TokenType::Comma),
+            '.' => self.make_token(TokenType::Dot),
+            '-' => self.make_token(TokenType::Minus),
+            '+' => self.make_token(TokenType::Plus),
+            '/' => self.make_token(TokenType::Slash),
+            '*' => self.make_token(TokenType::Star),
+            '!' if self.matches('=') => self.make_token(TokenType::BangEqual),
+            '!' => self.make_token(TokenType::Bang),
+            '=' if self.matches('=') => self.make_token(TokenType::EqualEqual),
+            '=' => self.make_token(TokenType::Equal),
+            '<' if self.matches('=') => self.make_token(TokenType::LessEqual),
+            '<' => self.make_token(TokenType::Less),
+            '>' if self.matches('=') => {
+                self.make_token(TokenType::GreaterEqual)
+            }
+            '>' => self.make_token(TokenType::Greater),
+            '"' => self.string(),
+            '0'..='9' => self.number(),
+            _ => self.error_token("Unexpected character."),
         }
     }
 
-    const fn is_at_end(&self) -> bool {
-        self.current == self.source.len()
+    fn advance(&mut self) -> Option<char> {
+        self.current += 1;
+        self.source.chars().nth(self.current - 1)
+    }
+
+    fn peek(&self) -> Option<char> {
+        self.source.chars().nth(self.current)
+    }
+
+    fn peek_next(&self) -> Option<char> {
+        self.source.chars().nth(self.current + 1)
+    }
+
+    fn matches(&mut self, expected: char) -> bool {
+        self.advance().map_or(false, |char| char == expected)
+    }
+
+    fn skip_whitespace(&mut self) {
+        loop {
+            match self.peek() {
+                Some(' ' | '\r' | '\t') => {
+                    self.advance();
+                }
+                Some('\n') => {
+                    self.line += 1;
+                    self.advance();
+                }
+                Some('/') if self.peek_next() == Some('/') => {
+                    while self.peek().is_some() {
+                        self.advance();
+                    }
+                }
+                _ => break,
+            }
+        }
     }
 
     fn make_token(&self, r#type: TokenType) -> Token<'src> {
@@ -98,5 +157,39 @@ impl<'src> Scanner<'src> {
             lexeme: message,
             line: self.line,
         }
+    }
+
+    fn string(&mut self) -> Token<'src> {
+        while self.peek() != Some('"') {
+            if self.peek() == Some('\n') {
+                self.line += 1;
+            }
+            self.advance();
+        }
+
+        if self.peek().is_none() {
+            self.error_token("Unterminated string.")
+        } else {
+            self.advance();
+            self.make_token(TokenType::String)
+        }
+    }
+
+    fn number(&mut self) -> Token<'src> {
+        while self.peek().is_some_and(char::is_numeric) {
+            self.advance();
+        }
+
+        if self.peek() == Some('.')
+            && self.peek_next().is_some_and(char::is_numeric)
+        {
+            self.advance();
+
+            while self.peek().is_some_and(char::is_numeric) {
+                self.advance();
+            }
+        }
+
+        self.make_token(TokenType::Number)
     }
 }
