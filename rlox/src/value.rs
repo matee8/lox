43312560be08ba2f1d624d08734af9ea64890efb
matee8 <molnarmatenorbert@gmail.com>
2025-02-02 20:52:06 +0100

    18.2: Lox Values and C Values - rlox

--- rlox/src/value.rs
+++ rlox/src/value.rs
@@ -1,1 +1,35 @@
-pub type Value = f64;
+pub enum Value {
+    Bool(bool),
+    Number(f64),
+    Nil,
+}
+
+impl Value {
+    pub const fn bool(value: bool) -> Self {
+        Self::Bool(value)
+    }
+
+    pub const fn number(value: f64) -> Self {
+        Self::Number(value)
+    }
+
+    pub const fn nil() -> Self {
+        Self::Nil
+    }
+
+    pub const fn as_bool(&self) -> Option<bool> {
+        if let Self::Bool(value) = *self {
+            Some(value)
+        } else {
+            None
+        }
+    }
+
+    pub const fn as_number(&self) -> Option<f64> {
+        if let Self::Number(value) = *self {
+            Some(value)
+        } else {
+            None
+        }
+    }
+}
